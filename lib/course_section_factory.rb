3286d9282eaa64b55de4abebde5ac0ef86c09225
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-06-09 18:09:48 -0400

    stubbed course_section_factory

---
+++ lib/course_section_factory.rb
@@ -0,0 +1,19 @@
+#File created 6/8/2022 by Jake McCann
+class Course_Section_Factory
+
+  # Created 6/8/2022 by Jake McCann
+  def initialize
+
+  end
+
+  # Created 6/8/2022 by Jake McCann
+  def courses(filter_parameters)
+
+  end
+
+  # Created 6/8/2022 by Jake McCann
+  def sections(filter_parameters)
+    
+  end
+
+end
