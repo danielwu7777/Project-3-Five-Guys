91169ffe0ece045a45935ab3966f095aeaa0e4b4
danielwu3412 <wu3412@buckeyemail.osu.edu>
2022-06-09 18:07:44 -0400

    Added user input/web wrapper ruby class files and their test files

--- lib/user_input.rb
+++ lib/user_input.rb
@@ -1,10 +1,16 @@
 # File created 6/8/2022 by Daniel Wu 
 
-class UserInput
+class User_Input
 
     # Created 6/8/2022 by Daniel Wu 
     def initialize
-
+        @filterParam
     end
 
+    # Created 6/9/2022 by Daniel Wu 
+    # Returns a hash of desired filters from the user(or an empty hash if no filters) 
+    def getFilters
+        puts "Here are the filters available:"
+        #Get all the filters from the user (or 0 for no filters)
+    end
 end
---
+++ spec/web_wrapper_spec.rb
@@ -0,0 +1,6 @@
+# File created 6/9/2022 by Daniel Wu 
+
+# File created 6/9/2022 by Daniel Wu 
+describe 'Web_Wrapper' do
+    
+end
