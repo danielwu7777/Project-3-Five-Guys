9b3ac28d303de4d8b881b76c0a9a0a082d87a449
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-06-10 13:59:13 -0400

    Implemented courses_html

--- lib/web_wrapper.rb
+++ lib/web_wrapper.rb
@@ -5,18 +5,20 @@ class Web_Wrapper
 
     # Created 6/9/2022 by Daniel Wu 
     def initialize
-        # Create mechanize client instance (should be a singleton, should never be instantiated more than once) 
+        # Create mechanize client instance (should be a singleton, should never be instantiated more than once)
+        @web_client = Mechanize.new
     end
 
     # Created 6/9/2022 by Daniel Wu
+    # Edited 6/9/2022 by Jake McCann: Implemented
     # Return string of entire web page with all courses at https://cse.osu.edu/courses 
-    def getCoursesPageString 
-
+    def courses_html
+        @web_client.get("https://cse.osu.edu/courses").body
     end
 
     # Created 6/9/2022 by Daniel Wu
-    # Returns string containing all HTML for sections of classNum at https://classes.osu.edu/class-search/#/
-    def getSectionPageString classNum
+    # Returns string containing all HTML for sections of class_num at https://classes.osu.edu/class-search/#/
+    def sections_html(class_num)
 
     end
 
