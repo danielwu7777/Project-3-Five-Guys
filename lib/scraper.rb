beeb803b402998d52e3a9607388a747772645e6f
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-06-11 17:08:16 -0400

    scrape_courses implementation

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,9 @@ class Scraper
   # Created 6/8/2022 by Jake McCann
   # filter_as_regex: regex representation of user filter parameters
   # html_to_scrape: html representation of page to be scraped
+
   def self.scrape(filter_as_regex, html_to_scrape)
     #get sections of html_to_scrape that follow filter_as_regex
+
   end
 end
