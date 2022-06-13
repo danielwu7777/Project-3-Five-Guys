9f65439707f46e8eeee935da709aff51eef74b9f
danielwu3412 <wu3412@buckeyemail.osu.edu>
2022-06-12 23:48:28 -0400

    in process of using TDD to implement regex_factory

--- lib/regex_factory.rb
+++ lib/regex_factory.rb
@@ -1,8 +1,22 @@
 # Created 6/8/2022 by Jake McCann
+# Edited 6/12/2022 by Daniel Wu
+
 class Regex_Factory
   # Created 6/8/2022 by Jake McCann
-  # filter_parameters: hash containing key-val pairs describing how user wants results filtered
-  def self.convert_to_regex(filter_parameters)
-    #Returns regex expression that is equal to filter parameters
+  # Edited 6/12/2022 by Daniel Wu: using TDD to implement method
+  # filter_parameters: hash containing key-val pairs describing how user wants courses filtered
+  def self.convert_course_filter_to_regex(filter_parameters)
+    case filter_parameters.keys[0]
+    when "num"
+      return /class=['|"]number['|"]>\(#{filter_parameters.values[0]}\)/
+    when "title"
+      return /class=['|"]title['|"]>.*#{filter_parameters.values[0]}/
+    when "descr"
+      return /class=['|"]label['|"]>Description.*#{filter_parameters.values[0]}/
+    when "pre"
+      return /Prereq:.*#{filter_parameters.values[0]}/
+    when "hrs"
+      return /class=['|"]label['|"]>.*Units:<\/span>.*#{filter_parameters.values[0]}/ 
+    end
   end
 end
--- spec/regex_factory_spec.rb
+++ spec/regex_factory_spec.rb
@@ -1,4 +1,61 @@
+require_relative '../lib/regex_factory'
 # File created 6/9/2022 by Jake McCann
+# Edited 6/11/2022 by Daniel Wu
 describe 'Regex_Factory' do
+    # Created 6/11/2022 by Daniel Wu
+    it 'filter course by class number' do
+      filterHash = Hash.new
+      filterHash = {"num" => "1110"}
+      result = Regex_Factory.convert_course_filter_to_regex (filterHash)
+      expected = /class=['|"]number['|"]>\(1110\)/
+      expect(result.to_s == expected.to_s).to be_truthy
+    end
 
+    # Created 6/12/2022 by Daniel Wu
+    it 'filter course by class title' do
+      filterHash = Hash.new
+      filterHash = {"title" => "Introduction to Computing"}
+      result = Regex_Factory.convert_course_filter_to_regex (filterHash)
+      expected = /class=['|"]title['|"]>.*Introduction to Computing/
+
+      expect(result.to_s == expected.to_s).to be_truthy
+    end
+
+    # Created 6/12/2022 by Daniel Wu
+    it 'filter course by class description' do
+      filterHash = Hash.new
+      filterHash = {"descr" => "A course of general interest giving"}
+      result = Regex_Factory.convert_course_filter_to_regex (filterHash)
+      expected = /class=['|"]label['|"]>Description.*A course of general interest giving/
+      expect(result.to_s == expected.to_s).to be_truthy
+    end
+
+    # Created 6/12/2022 by Daniel Wu
+    it 'filter course by class prereqs' do
+      filterHash = Hash.new
+      filterHash = {"pre" => "Not open to students with credit for 1112"}
+      result = Regex_Factory.convert_course_filter_to_regex (filterHash)
+      expected =/Prereq:.*Not open to students with credit for 1112/
+      expect(result.to_s == expected.to_s).to be_truthy
+    end
+
+    # Created 6/12/2022 by Daniel Wu
+    it 'filter course by class hours' do
+      filterHash = Hash.new
+      filterHash = {"hrs" => "3"}
+      result = Regex_Factory.convert_course_filter_to_regex (filterHash)
+      expected = /class=['|"]label['|"]>.*Units:<\/span>.*3/ 
+      expect(result.to_s == expected.to_s).to be_truthy
+    end
+
+=begin
+    # Created 6/11/2022 by Daniel Wu
+    it 'filter course by class number and class hours' do
+      filterHash = Hash.new
+      filterHash = {"num" => "1110", "hrs" => "3"}
+      result = Regex_Factory.convert_course_filter_to_regex (filterHash)
+      expected = #TODO
+      expect(result.to_s == expected.to_s).to be_truthy
+    end
+=end
 end
