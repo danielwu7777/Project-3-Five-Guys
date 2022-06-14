a8bd111e5e3117322e9822d5bd269101606be277
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-06-14 14:58:28 -0400

    refactored safe input

--- lib/safe_input.rb
+++ lib/safe_input.rb
@@ -1,31 +1,13 @@
 #Created 6/12/2022 by Jake McCann
 #File edited 6/14/2022 by Jake McCann
 class Safe_Input
-
   # Created 6/12/2022 by Jake McCann
-  # Modified 6/14/2022 by Jake McCann consolidating safe input into one method
-  # Gets and returns user menu choice
-  # options: array of menu options that are valid
-  def self.menu_choice(options)
-    input = STDIN.gets.chomp
-    while !options.include? input do
-      yield if block_given?
-      input = STDIN.gets.chomp
-    end
-    input
-  end
-
-  # Created 6/12/2022 by Jake McCann
-  # Gets and returns hash containing filter parms
-  def self.filter
-    #TODO: implement after figuring out what filter params need to look like
-  end
-
-  # Created 6/12/2022 by Jake McCann
+  # Modified 6/14/2022 by Jake McCann consolidating safe input
   # Gets and returns valid file path
-  def self.output_file
+  # regex: regex expression used to determine what is valid input
+  def self.safe_input regex
     input = STDIN.gets.chomp
-    while !/^(\/[a-z_\-\s0-9\.]+)*\w+\.html/.match?(input)
+    while !regex.match?(input)
       yield if block_given?
       input = STDIN.gets.chomp
     end
