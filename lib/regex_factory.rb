4a0065a80ddfa0833fe91a5e6e73c3cd657b4a7c
danielwu3412 <wu3412@buckeyemail.osu.edu>
2022-06-13 22:43:15 -0400

    refactored course regex_factory

--- lib/regex_factory.rb
+++ lib/regex_factory.rb
@@ -6,40 +6,25 @@ class Regex_Factory
   # Edited 6/12/2022 by Daniel Wu: using TDD to implement method
   # Edited 6/13/2022 by Daniel Wu: added ability to iterate through hash
   # filter_parameters: hash containing key-val pairs describing how user wants courses filtered
-  def self.convert_course_filter_to_regex(filter_parameters)
+  def self.convert_course_filter_to_regex filter_parameters
     regExpString = ""
     # Counter to know when at the last key-val pair in hash
     counter = 1
     filter_parameters.each do |key, value|
       case key
-      when "num"
-        # If statement determines whether to append .* at the end of regExpString
-        if counter == filter_parameters.size then 
-          regExpString += "class=[\'|\"]number[\'|\"]>\\(#{value}\\)"
-        else regExpString += "class=[\'|\"]number[\'|\"]>\\(#{value}\\).*"
-        end  
-      when "title"
-        if counter == filter_parameters.size then 
-          regExpString += "class=[\'|\"]title[\'|\"]>.*#{value}"
-        else regExpString += "class=[\'|\"]title[\'|\"]>.*#{value}.*"
-        end  
-      when "descr"
-        if counter == filter_parameters.size then 
-          regExpString += "class=[\'|\"]label[\'|\"]>Description.*#{value}"
-        else 
-          regExpString += "class=[\'|\"]label[\'|\"]>Description.*#{value}.*"
-        end  
-      when "pre"
-        if counter == filter_parameters.size then 
-          regExpString += "Prereq:.*#{value}"
-        else regExpString += "Prereq:.*#{value}.*"
-        end  
-      when "hrs"
-        if counter == filter_parameters.size then 
-          regExpString += "class=[\'|\"]label[\'|\"]>.*Units:<\/span>.*#{value}"
-        else regExpString += "class=[\'|\"]label[\'|\"]>.*Units:<\/span>.*#{value}.*"
-        end 
+        when "num"
+            regExpString += "class=[\'|\"]number[\'|\"]>\\(#{value}\\)" 
+        when "title"
+            regExpString += "class=[\'|\"]title[\'|\"]>.*#{value}"
+        when "descr"
+            regExpString += "class=[\'|\"]label[\'|\"]>Description.*#{value}"
+        when "pre"
+            regExpString += "Prereq:.*#{value}"
+        when "hrs"
+            regExpString += "class=[\'|\"]label[\'|\"]>.*Units:<\/span>.*#{value}"
       end
+      # Determines whether to append .* at the end of regExpString
+      if counter != filter_parameters.size then regExpString += ".*" end
       counter += 1
     end
     return Regexp.new(regExpString)
