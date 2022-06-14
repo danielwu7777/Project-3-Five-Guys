ce2205e21862c86d1b60007b2ae18ad26e7db566
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-06-14 15:21:53 -0400

    fix comment

--- lib/safe_input.rb
+++ lib/safe_input.rb
@@ -3,7 +3,7 @@
 class Safe_Input
   # Created 6/12/2022 by Jake McCann
   # Modified 6/14/2022 by Jake McCann consolidating safe input
-  # Gets and returns valid file path
+  # Gets and returns valid input based on regex
   # regex: regex expression used to determine what is valid input
   def self.safe_input regex
     input = STDIN.gets.chomp
