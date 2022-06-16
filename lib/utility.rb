6bacc3c3be41fe2259076bee880206f10b6776a7
Yan1378 <yan.1378@osu.edu>
2022-06-16 19:56:06 -0400

    parentheses removed
    remove parentheses from user_input.rb and utility.rb

--- lib/utility.rb
+++ lib/utility.rb
@@ -1,12 +1,13 @@
 # File created 6/11/2022 by Noah Moon
 # File edited 6/12/2022 by Noah Moon
+# Edited 6/16/2022 by Yuhao Yan: parentheses removed
 require 'set'
 
 class Utility
   # Created 6/11/2022 by Noah Moon
   # Edited 6/12/2022 by Noah Moon
   # Returns a new string which wraps line according to max_line
-  def self.wrap_string(string, max_line, break_chars = Set[" "])
+  def self.wrap_string string, max_line, break_chars = Set[" "]
     newString = String.new
 
     string.each_line do |line|
