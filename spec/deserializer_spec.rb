06e54146ddb32ef7c08ae5b7aa14fc95a2611c28
Yan1378 <yan.1378@osu.edu>
2022-06-15 21:12:05 -0400

    More test case

--- spec/deserializer_spec.rb
+++ spec/deserializer_spec.rb
@@ -5,7 +5,10 @@ require_relative "../lib/deserializer"
 sections = Array.new
 section1 = Section.new "12345", "fall", "online", "Columbus", "Gym", "43212", "M TH F", "9:00 - 23:00", "PE 3901"
 section2 = Section.new "23456", "summer", "face-to-face", "Columbus", "Kroger", "AISLE 4 ", "M T W F", "8:30 - 23:50"
-sections.push section1, section2
+section3 = Section.new "23456", "summer", "face-to-face", "Columbus", "Kroger", "AISLE 4 ", "M T W F", "8:30 - 23:50"
+section4 = Section.new "23456", "summer", "face-to-face", "Columbus", "Kroger", "AISLE 4 ", "M T W F", "8:30 - 23:50"
+section5 = Section.new "23456", "summer", "face-to-face", "Columbus", "Kroger", "AISLE 4 ", "M T W F", "8:30 - 23:50"
+sections.push section1, section2, section3, section4, section5
 
 Deserializer.print_file sections,"../html_testing.html"
 
