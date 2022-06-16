073021bfde6358c0bddcfa8a4dcf72f44e783509
Yan1378 <yan.1378@osu.edu>
2022-06-16 19:32:54 -0400

    parentheses removed
    parentheses removed from course_section_factory.rb

--- lib/course_section_factory.rb
+++ lib/course_section_factory.rb
@@ -15,14 +15,14 @@ class Course_Section_Factory
 
   # Created 6/9/2022 by Jake McCann
   # Edited 6/10/2022 by Noah Moon
-  def courses(filter_parameters)
+  def courses filter_parameters
     # fetches and print html page of sections
     Serializer.serialize_courses Scraper.scrape_courses filter_parameters, @web.courses_html
   end
 
   # Created 6/9/2022 by Jake McCann
   # Edited 6/10/2022 by Noah Moon
-  def sections(filter_parameters)
+  def sections filter_parameters
     # Return array of section objects (array for order)
   end
 
